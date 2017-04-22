0d6fa261eba7ddb729257cc8c9db99bef563bd98
sabari <sabari>
2017-04-23 03:40:42 +0530

    second commit

---
+++ app/src/main/java/me/sabarirangan/apps/findpeoples/extras/OnLoadMoreListener.java
@@ -0,0 +1,9 @@
+package me.sabarirangan.apps.findpeoples.extras;
+
+/**
+ * Created by sabari on 21-04-2017.
+ */
+
+public interface OnLoadMoreListener {
+    void onLoadMore();
+}
---
+++ app/src/main/java/me/sabarirangan/apps/findpeoples/model/NewTag.java
@@ -0,0 +1,14 @@
+package me.sabarirangan.apps.findpeoples.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by sabari on 22-04-2017.
+ */
+
+public class NewTag {
+
+}
